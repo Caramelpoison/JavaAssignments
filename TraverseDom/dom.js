ed0f1acd26257354e8b0c4d5eb145d8bf685c01e
caramel <AlexanderForward149@gmail.com>
2023-04-19 21:30:51 -0400

    traverse Dom

---
+++ TraverseDom/dom.js
@@ -0,0 +1,35 @@
+//get the header tags
+var header = document.getElementsByTagName("header");
+
+//get all the sections
+var sections = document.getElementsByTagName('section');
+
+//get the class "current"
+var currentSect = document.querySelector('section.current');
+
+//Next section after current section 
+var nextSect = currentSect.nextElementSibling;
+
+//Previous section of current section
+var previousSect = currentSect.previousElementSibling;
+
+//Get the highlight class
+var highlight =  document.getElementsByClassName('highlight');
+
+var allH2 = document.querySelectorAll("section h2");
+
+document.querySelector
+
+console.log(header)
+console.log("")
+console.log(sections)
+console.log("")
+console.log(currentSect)
+console.log("")
+console.log(nextSect)
+console.log("")
+console.log(previousSect)
+console.log("")
+console.log(highlight)
+console.log("")
+console.log(allH2)
